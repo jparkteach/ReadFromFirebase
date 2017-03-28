173b4bd4c206f6e7ef5962dec35498bc79ad0bdc
John <jparkteach@yahoo.com>
2017-03-27 20:14:33 -0400

    ReadFromFirebase

---
+++ app/src/main/java/net/jptechnology/android/readfromfirebase/Person.java
@@ -0,0 +1,33 @@
+package net.jptechnology.android.readfromfirebase;
+
+public class Person {
+    private String name;
+    private int age;
+    private boolean alive;
+
+    public Person() {
+    } // Empty constructor is needed to read from Firebase
+
+    public Person(String name, int age, boolean alive) {
+        this.name = name;
+        this.age = age;
+        this.alive = alive;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public boolean isAlive() {
+        return alive;
+    }
+
+    @Override
+    public String toString() {
+        return name + " " + " " + age + " " + alive;
+    }
+}
